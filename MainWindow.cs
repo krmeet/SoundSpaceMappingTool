c626c57b031fee939fa070ebcd596de7e8d8cba5
krmeet <jayden@cumallover.me>
2021-03-03 09:25:57 +0000

    just like old times

--- MainWindow.cs
+++ MainWindow.cs
@@ -1,22 +1,36 @@
 using System;
 using OpenTK;
+using OpenTK.Mathematics;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
 namespace SoundSpaceMappingTool
 {
-    public class MainWindow : GameWindow
-    {
-        public MainWindow(int width, int height, string title, VSyncMode vsync) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
-        {
-            this.Title = title;
-            this.VSync = vsync;
-            this.RenderFrame += new Action<FrameEventArgs>(Render);
-        }
-        public void Render(FrameEventArgs args)
-        {
-            GL.ClearColor(0,0,0,1);
-        }
-    }
+	public class MainWindow : GameWindow
+	{
+		public MainWindow(int width, int height, string title, VSyncMode vsync) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+		{
+			Title = title;
+			Size = new Vector2i(width, height);
+			VSync = vsync;
+		}
+		protected override void OnLoad()
+		{
+			GL.ClearColor(0, 0, 0, 1);
+			base.OnLoad();
+		}
+		protected override void OnResize(ResizeEventArgs e)
+		{
+			GL.Viewport(0, 0, Size.X, Size.Y);
+			base.OnResize(e);
+		}
+		protected override void OnRenderFrame(FrameEventArgs args)
+		{
+			GL.Clear(ClearBufferMask.ColorBufferBit);
+
+			Context.SwapBuffers();
+			base.OnRenderFrame(args);
+		}
+	}
 }
