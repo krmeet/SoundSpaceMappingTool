3c3b90dc6e08d7fbe4ae1a51da1d1a06c312a248
krmeet <jayden@cumallover.me>
2021-03-16 21:48:55 +0000

    awooga parents and more frame stuff

--- GUI/GuiFrame.cs
+++ GUI/GuiFrame.cs
@@ -5,15 +5,26 @@ namespace GUI
 {
 	public class GuiFrame : GuiObject
 	{
+		public bool Visible = true;
 		public int ZIndex = 0;
 		public Color4 Colour = new Color4(1, 1, 1, 1);
+		public bool MouseOver { get; protected set; }
 		public GuiFrame() : base() { }
 		public GuiFrame(Vector4 p, Vector4 s) : base(p, s) { }
 		public override void Render(FrameEventArgs e)
 		{
-			GL.Color4(Colour);
-			GLR.RenderRect(Rect, ZIndex);
-			base.Render(e);
+			if (Visible && Colour.A >= 0.05)
+			{
+				GL.Color4(Colour);
+				GLR.RenderRect(Rect, ZIndex);
+				base.Render(e);
+			}
 		}
+		public virtual void OnMouseEnter()
+		{ }
+		public virtual void OnMouseLeave()
+		{ }
+		public virtual void OnMouseMove()
+		{ }
 	}
 }
--- GUI/GuiObject.cs
+++ GUI/GuiObject.cs
@@ -9,6 +9,7 @@ namespace GUI
 		public Vector2 AbsSize { get; private set; }
 		public Vector4 Position;
 		public Vector4 Size;
+		public GuiObject Parent;
 		protected RectangleF Rect;
 		protected MainWindow Window;
 		public GuiObject()
@@ -34,15 +35,20 @@ namespace GUI
 		{ }
 		public virtual void OnResize()
 		{
-			var viewport = Window.ClientRectangle;
+			Rectangle viewport = Window.ClientRectangle;
+			if (Parent != null)
+			{
+				var pRect = Parent.Rect;
+				viewport = new Rectangle((int)pRect.X, (int)pRect.Y, (int)pRect.Width, (int)pRect.Height);
+			}
 			var center = new PointF(viewport.Width / 2, viewport.Height / 2);
 			var x = (int)(Position.X * viewport.Width) + (int)Position.Y;
 			var y = (int)(Position.Z * viewport.Height) + (int)Position.W;
 			int w = (int)(Size.X * viewport.Width) + (int)Size.Y;
 			int h = (int)(Size.Z * viewport.Height) + (int)Size.W;
-			AbsPosition = new Vector2(x, y);
+			AbsPosition = new Vector2(x + viewport.X, y + viewport.Y);
 			AbsSize = new Vector2(w, h);
-			Rect = new RectangleF(x, y, w, h);
+			Rect = new RectangleF(x + viewport.X, y + viewport.Y, w, h);
 		}
 	}
 }
